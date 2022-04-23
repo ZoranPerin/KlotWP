3632762a70d812952b88ddfea5e9d507c5cfef70
Zoran Perin <perinzoran@gmail.com>
2022-04-23 09:58:49 +0200

    UPDATE: gulp version

--- wp-content/themes/klot/gulpfile.js
+++ wp-content/themes/klot/gulpfile.js
@@ -1,12 +1,11 @@
 // dependencies
-var gulp = require('gulp');
-var merge = require('merge-stream');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var autoprefixer = require('gulp-autoprefixer');
-var cssnano = require('gulp-cssnano');
-var uglify = require('gulp-uglify');
-var gcmq = require('gulp-group-css-media-queries');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const uglify = require('gulp-uglify');
+const gcmq = require('gulp-group-css-media-queries');
 
 // config
 config = {
@@ -14,43 +13,54 @@ config = {
     destination: './assets/'
 }
 
-// sass
-gulp.task('scss', function () {
-    return gulp.src(config.resources + 'sass/**/*.scss')
+// images
+function images() {
+    return gulp
+        .src(config.resources + 'images/**/*')
+        .pipe(gulp.dest(config.destination + 'images'));
+}
+
+// css
+function css() {
+    return gulp
+        .src(config.resources + 'sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gcmq())
         .pipe(cssnano())
         .pipe(autoprefixer())
         .pipe(gulp.dest(config.destination + 'css'))
-});
+}
 
-// js
-gulp.task('js', function () {
-    return gulp.src([config.resources + 'js/vendor/*.js', config.resources + 'js/*.js'])
+// javascript
+function scripts() {
+    return gulp
+		.src([config.resources + 'js/vendor/*.js', config.resources + 'js/*.js'])
         .pipe(concat('scripts.js'))
         .pipe(uglify())
         .pipe(gulp.dest(config.destination + 'js'));
-});
-
-// images
-gulp.task('images', function () {
-    return gulp.src(config.resources + 'images/**/*')
-        .pipe(gulp.dest(config.destination + 'images'))
-});
+}
 
 // fonts
-gulp.task('fonts', function () {
-    return gulp.src(config.resources + 'fonts/**/*')
+function fonts() {
+    return gulp
+		.src(config.resources + 'fonts/**/*')
         .pipe(gulp.dest(config.destination + 'fonts'))
-});
-
-// default
-gulp.task('default', ['js', 'scss', 'images', 'fonts']);
+}
 
 // watch
-gulp.task('watch', function () {
-    gulp.watch('resources/sass/**/*.scss', ['scss']);
-    gulp.watch('resources/js/**/*.js', ['js']);
-    gulp.watch('resources/images/**/*', ['images']);
-    gulp.watch('resources/fonts/**/*', ['fonts']);
-});
+function watch() {
+    gulp.watch('resources/sass/**/*.scss', css);
+    gulp.watch('resources/js/**/*.js', scripts);
+    gulp.watch('resources/images/**/*', images);
+    gulp.watch('resources/fonts/**/*', fonts);
+}
+
+const build = gulp.parallel(css, images, fonts, scripts);
+
+// export tasks
+exports.images = images;
+exports.css = css;
+exports.scripts = scripts;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
